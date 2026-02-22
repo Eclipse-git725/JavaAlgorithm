e1a4a9855b860e77a1157822ecf98ea5eca8f45b
gxy <2219570448@qq.com>
2026-02-22 19:36:06 +0800

    2/16~2/22 力扣题

---
+++ src/leetcode/editor/cn/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,63 @@
+//给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 5
+//输出：true
+//解释：5 的二进制表示是：101
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 7
+//输出：false
+//解释：7 的二进制表示是：111. 
+//
+// 示例 3： 
+//
+// 
+//输入：n = 11
+//输出：false
+//解释：11 的二进制表示是：1011. 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 2³¹ - 1 
+// 
+//
+// Related Topics 位运算 👍 263 👎 0
+
+
+package leetcode.editor.cn;
+
+public class BinaryNumberWithAlternatingBits {
+    public static void main(String[] args) {
+        Solution solution = new BinaryNumberWithAlternatingBits().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean hasAlternatingBits(int n) {
+            int last = n & 1;
+            n >>= 1;
+            while(n != 0) {
+                if((n & 1) != last) {
+                    last = n & 1;
+                }else {
+                    return false;
+                }
+                n >>= 1;
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/ReverseBits.java
@@ -0,0 +1,94 @@
+//颠倒给定的 32 位有符号整数的二进制位。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：n = 43261596 
+// 
+//
+// 输出：964176192 
+//
+// 解释： 
+//
+// 
+// 
+// 
+// 整数 
+// 二进制 
+// 
+// 
+// 43261596 
+// 00000010100101000001111010011100 
+// 
+// 
+// 964176192 
+// 00111001011110000010100101000000 
+// 
+// 
+// 
+//
+// 示例 2： 
+//
+// 
+// 输入：n = 2147483644 
+// 
+//
+// 输出：1073741822 
+//
+// 解释： 
+//
+// 
+// 
+// 
+// 整数 
+// 二进制 
+// 
+// 
+// 2147483644 
+// 01111111111111111111111111111100 
+// 
+// 
+// 1073741822 
+// 00111111111111111111111111111110 
+// 
+// 
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= n <= 2³¹ - 2 
+// n 为偶数 
+// 
+//
+// 
+//
+// 进阶: 如果多次调用这个函数，你将如何优化你的算法？ 
+//
+// Related Topics 位运算 分治 👍 770 👎 0
+
+  
+package leetcode.editor.cn;
+public class ReverseBits{
+  public static void main(String[] args) {
+       Solution solution = new ReverseBits().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+        for(int i = 0; i < 32; i ++) {
+            int c = n & 1;
+            ans = (ans << 1) + c;
+            n >>= 1;
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
