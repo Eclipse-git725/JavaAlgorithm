d568b9f08ac0db463eafae747581bfb15d21bd1a
gxy <2219570448@qq.com>
2026-05-03 01:11:07 +0800

    4/27~5/3 力扣题

---
+++ src/leetcode/editor/cn/MinimumOperationsToMakeAUniValueGrid.java
@@ -0,0 +1,96 @@
+//给你一个大小为 m x n 的二维整数网格 grid 和一个整数 x 。每一次操作，你可以对 grid 中的任一元素 加 x 或 减 x 。 
+//
+// 单值网格 是全部元素都相等的网格。 
+//
+// 返回使网格化为单值网格所需的 最小 操作数。如果不能，返回 -1 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：grid = [[2,4],[6,8]], x = 2
+//输出：4
+//解释：可以执行下述操作使所有元素都等于 4 ： 
+//- 2 加 x 一次。
+//- 6 减 x 一次。
+//- 8 减 x 两次。
+//共计 4 次操作。
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：grid = [[1,5],[2,3]], x = 1
+//输出：5
+//解释：可以使所有元素都等于 3 。
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 
+//输入：grid = [[1,2],[3,4]], x = 2
+//输出：-1
+//解释：无法使所有元素相等。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 10⁵ 
+// 1 <= m * n <= 10⁵ 
+// 1 <= x, grid[i][j] <= 10⁴ 
+// 
+//
+// Related Topics 数组 数学 矩阵 排序 👍 67 👎 0
+
+  
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+public class MinimumOperationsToMakeAUniValueGrid{
+  public static void main(String[] args) {
+       Solution solution = new MinimumOperationsToMakeAUniValueGrid().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        int m = grid.length, n = grid[0].length;
+        int[] nums = new int[m * n];
+        int cnt = 0;
+        if(m * n == 1) {
+            return 0;
+        }
+        for(int i = 0; i < m; i ++ ) {
+            for(int j = 0; j < n; j ++ ) {
+                nums[cnt ++] = grid[i][j];
+            }
+        }
+
+        Arrays.sort(nums);
+        int mid = 0;
+        mid = nums[m * n / 2];
+        int ans = 0;
+        for(int i = 0; i < m * n; i ++ ) {
+            if((Math.abs(mid - nums[i])) % x != 0) {
+                return -1;
+            }
+            ans += (Math.abs(mid - nums[i])) / x;
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
